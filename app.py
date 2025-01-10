26c19d298d64d788e47c599448803e941f4ae42d
kaushikgunaware <gunawarekaushik1610@gmail.com>
2025-01-10 22:22:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import nltk 
+nltk.download('punket')
+nltk.download('punket_tab')
+nltk.download('stopwords')
+
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStremmer
+
+ps = PorterStemmer()
+
+def tranform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum() :
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tk = pickle.load(open("vectorizer.pkl", 'rb'))
+model = pickle.load(open("model.pkl", 'rb'))
+
+st.title("SMS Spam Detection Model")
+st.write("Made by Kaushik Gunaware*")
+
+input_sms = st.text_input("Enter the SMS")
+
+if st.button('Predict'):
+    # 1.preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2.vectorize
+    vector_input = tk.transform([transformed_sms])
+    # 3.predict
+    result = model.predict(vector_input)[0]
+    # 4.Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
